e460393ffe4f5f646a523968cb310f38aa3ad369
tranntommy <tranntommy@yahoo.com>
2020-01-16 11:24:49 -0600

    Create hello.c
    Added major

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Name: Truong Tran
+Date: 2020/1/16
+Tis program prints out my name and major
+*/
+
+#include <stdio.h>
+#iclude <stdlib.h>
+
+int main(void)
+{
+	printf("Truong Tran!\n");
+	printf("Electrical Engineer\n");
+	return 0;
+}
